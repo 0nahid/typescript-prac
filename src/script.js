5ad3ca39510c4765d84b6572c6203f016c1b6cf1
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-08-12 16:17:27 +0600

    interface

---
+++ src/script.js
@@ -0,0 +1,22 @@
+// function log(any: any): void {
+//   return console.log(any);
+// }
+// // function multiply(a: number, b: number) {
+// //   return a * b;
+// // }
+// // log(multiply(2, 3));
+var person1 = {
+    name: "Tamim",
+    age: 22,
+    adult: true,
+    hobbies: ["Sleeping", "Reading"]
+};
+var person2 = {
+    name: "Nahid",
+    age: 4,
+    adult: true,
+    hobbies: ["Coding", "Cooking"]
+};
+// console.log(person1);
+var personArray = [person1, person2];
+console.log(personArray);
--- src/script.ts
+++ src/script.ts
@@ -1,38 +1,38 @@
-function log(any: any): void {
-  return console.log(any);
-}
-// function multiply(a: number, b: number) {
-//   return a * b;
+// function log(any: any): void {
+//   return console.log(any);
 // }
-// log(multiply(2, 3));
+// // function multiply(a: number, b: number) {
+// //   return a * b;
+// // }
+// // log(multiply(2, 3));
 
-// // Array
-// let fruits = ["apple", "mango", "orange"];
-// fruits.push(1)
-// let mixed = [1, "Nahid", true ,{}];
-// mixed.push(1);
-// mixed.push({
-//   name:'Nahid'
-// })
+// // // Array
+// // let fruits = ["apple", "mango", "orange"];
+// // fruits.push(1)
+// // let mixed = [1, "Nahid", true ,{}];
+// // mixed.push(1);
+// // mixed.push({
+// //   name:'Nahid'
+// // })
 
-// // Object
-// let person = {
-//   name:'Nahid',
-//   class:12,
-//   isCoder:true,
-// }
-// person.name = 'Nahid Hassan';
+// // // Object
+// // let person = {
+// //   name:'Nahid',
+// //   class:12,
+// //   isCoder:true,
+// // }
+// // person.name = 'Nahid Hassan';
 
-// // Explicit & Union Types
-// let a: string;
-// let b: number;
-// array
-// let a: (string | number)[] = [];
-// a.push('Nahid', 4 , true);
-// let b: string | number;
-// b = "Nahid";
-// b = 1;
-// object
+// // // Explicit & Union Types
+// // let a: string;
+// // let b: number;
+// // array
+// // let a: (string | number)[] = [];
+// // a.push('Nahid', 4 , true);
+// // let b: string | number;
+// // b = "Nahid";
+// // b = 1;
+// // object
 // let c: {
 //   name: string;
 //   age: number;
@@ -44,19 +44,35 @@ function log(any: any): void {
 //   adult: true,
 // };
 
-// // function
-// const myFunc = (a: string, b: string, c?: string) => {
-//   log(`Hello ${a} ${b}`);
+// // type aliases
+// type stringOrNumber = string | number;
+// type userType = { name: string; password?: stringOrNumber; age: number };
+// const userDetails = (id: stringOrNumber, user: userType) => {
+//   log(`User id id ${id} , name is ${user.name} & age is ${user.age}`);
 // };
-// myFunc("world", '!');
 
-// type aliases
-type stringOrNumber = string | number;
-type userType = { name: string; password?: stringOrNumber; age: number };
-const userDetails = (id: stringOrNumber, user: userType) => {
-  log(`User id id ${id} , name is ${user.name} & age is ${user.age}`);
-};
+// const sayHello = (user: userType) => {
+//   log(`Hello ${user.age > 50 ? "Sir" : "Mr."} ${user.name}`);
+// };
 
-const sayHello = (user: userType) => {
-  log(`Hello ${user.age > 50 ? "Sir" : "Mr."} ${user.name}`);
+interface IPerson {
+  name: string;
+  age: number;
+  adult: boolean;
+  hobbies?: string[];
+}
+const person1: IPerson = {
+  name: "Tamim",
+  age: 22,
+  adult: true,
+  hobbies: ["Sleeping", "Reading"],
 };
+const person2: IPerson = {
+  name: "Nahid",
+  age: 4,
+  adult: true,
+  hobbies: ["Coding", "Cooking"],
+};
+// console.log(person1);
+const personArray: IPerson[] = [person1, person2];
+console.log(personArray);
